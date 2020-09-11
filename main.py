c491565f048caec95ae034c95ae6c4f3b57cb858
lby0615 <bjl5716@psu.edu>
2020-09-11 04:57:22 +0000

    return format change

--- main.py
+++ main.py
@@ -2,23 +2,23 @@
 # solo
 def getGradePoint(grade):
   if grade == "A": 
-    return("4.0")
+    return 4.0
   elif grade == "A-":
-    return("3.67")
+    return 3.67
   elif grade == "B+":
-    return("3.33")
+    return 3.33
   elif grade == "B":
-    return("3.0")
+    return 3.0
   elif grade == "B-":
-    return("2.67")
+    return 2.67
   elif grade == "C+":
-    return("2.33")
+    return 2.33
   elif grade == "C":
-    return("2.0")
+    return 2.0
   elif grade == "D":
-    return("1.0")
+    return 1.0
   else:
-    return("0.0")
+    return 0.0
 
 def run():
   grade1 = input("Enter your course 1 letter grade: ")
