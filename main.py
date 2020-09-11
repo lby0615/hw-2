1b8757277d1dd04effb2cfe328520ce8155c06c8
lby0615 <bjl5716@psu.edu>
2020-09-11 04:54:10 +0000

    DELETE A COMMA

--- main.py
+++ main.py
@@ -24,20 +24,20 @@ def run():
   grade1 = input("Enter your course 1 letter grade: ")
   credit1 = float(input("Enter your course 1 credit:  "))
   point1 = float(getGradePoint(grade1))
-  print (f"Grade point for course 1 is {getGradePoint(grade1)}.")
+  print (f"Grade point for course 1 is {getGradePoint(grade1)}")
 
   grade2 = input("Enter your course 2 letter grade: ")
   credit2 = float(input("Enter your course 2 credit:  "))
   point2 = float(getGradePoint(grade2))
-  print (f"Grade point for course 2 is {getGradePoint(grade2)}.")
+  print (f"Grade point for course 2 is {getGradePoint(grade2)}")
 
   grade3 = input("Enter your course 3 letter grade: ")
   credit3 = float(input("Enter your course 3 credit:  "))
   point3 = float(getGradePoint(grade3))
-  print (f"Grade point for course 3 is {getGradePoint(grade3)}.")
+  print (f"Grade point for course 3 is {getGradePoint(grade3)}")
 
   GPA = ((point1*credit1)+(point2*credit2)+(point3*credit3))/(credit1+credit2+credit3)
-  print (f"Your GPA is: {GPA}.")
+  print (f"Your GPA is: {GPA}")
  
 if __name__ == "__main__":
   run()
